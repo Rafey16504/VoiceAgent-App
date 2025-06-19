fab6f2149592f9ccca66250cdaea9cec590d44bf
Abdul Rafey <abdulrafey16504@gmail.com>
2025-06-19 22:03:15 +0500

    Changed avatar visuals

--- components/avatar/avatarVisualizer.tsx
+++ components/avatar/avatarVisualizer.tsx
@@ -9,17 +9,28 @@ export function AvatarVisualizer({ track }: { track?: RemoteAudioTrack }) {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="w-48 h-48 bg-gray-200 rounded-full relative flex items-center justify-center shadow-lg">
-        {/* Eyes */}
+      <div className="w-48 h-48 bg-gray-200 rounded-t-full relative flex items-center justify-center shadow-lg">
+        {/* Eyebrows */}
+        {/* <div className="absolute top-1/4 right-1/4 w-7 h-1 bg-black rounded-ss-full rounded-se-full" 
+        style={{
+            // width: `${mouthOpen * 5 + 20}px `,
+            height: `${mouthOpen * -2 + 4}px`,
+          }}/>
+        <div className="absolute top-1/4 left-1/4 w-7 h-1 bg-black rounded-ss-full rounded-se-full" 
+        style={{
+            // width: `${mouthOpen * 5 + 20}px `,
+            height: `${mouthOpen * -2 + 4}px`,
+          }}/> */}
+          {/* Eyes */}
         <div className="absolute top-1/3 left-1/4 w-4 h-4 bg-black rounded-full" />
         <div className="absolute top-1/3 right-1/4 w-4 h-4 bg-black rounded-full" />
         
         {/* Mouth */}
         <div
-          className="absolute bottom-[30%] bg-black rounded-full transition-all duration-100"
+          className="absolute bottom-[30%] bg-black rounded-b-3xl transition-all duration-100"
           style={{
-            width: `${mouthOpen * 50 + 20}px`,
-            height: `${mouthOpen * 40 + 20}px`,
+            width: `${mouthOpen * -10 + 15}px `,
+            height: `${mouthOpen * 30 + 5}px`,
           }}
         />
       </div>
